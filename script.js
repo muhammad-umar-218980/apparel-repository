f5240ed6e862c5476932ffcc1967443c37451588
MuhammadUmar1502 <muhammad.umar.218980@gmail.com>
2024-02-26 02:11:18 +0500

    changes added

---
+++ script.js
@@ -0,0 +1,14 @@
+ var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+  var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+  });
+
+  
+  document.getElementById("dropdown-toggle").addEventListener("click", function() {
+    var menu = document.getElementById("dropdown-menu");
+    if (menu.style.display === "none" || menu.style.display === "") {
+      menu.style.display = "block";
+    } else {
+      menu.style.display = "none";
+    }
+  });
